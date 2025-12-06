989574cac1f5893cf7a0d12061a1c9ff7390de4b
Linglinglau <117104001+Linglinglau@users.noreply.github.com>
2025-12-06 23:23:17 +0100

    smoother transitions upon navigationw

--- script/script.js
+++ script/script.js
@@ -5,3 +5,24 @@ $(function () {
         $(this).load(file)
     })
 })
+
+$(function () {
+    $('[data-include]').each(function () {
+        const file = $(this).data('include') + '.html';
+        $(this).load(file);
+    });
+});
+
+$(document).on('click', '.nav-link', function (e) {
+    e.preventDefault();
+    const page = $(this).data('page');
+    loadPage(page + '.html');
+});
+
+function loadPage(file) {
+    $('#content').load(file);
+}
+
+$(window).on('load', function () {
+    loadPage('aboutme.html');
+});
