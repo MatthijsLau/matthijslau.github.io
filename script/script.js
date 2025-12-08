0f7fff92c2db052b8357975b37ce85f52348be27
Linglinglau <117104001+Linglinglau@users.noreply.github.com>
2025-12-08 18:04:25 +0100

    Added random banners

--- script/script.js
+++ script/script.js
@@ -1,11 +1,4 @@
-$(function () {
-    var includes = $('[data-include]')
-    $.each(includes, function () {
-        var file = $(this).data('include') + '.html'
-        $(this).load(file)
-    })
-})
-
+// Load HTML snippets from data-include attributes
 $(function () {
     $('[data-include]').each(function () {
         const file = $(this).data('include') + '.html';
@@ -13,16 +6,33 @@ $(function () {
     });
 });
 
-$(document).on('click', '.nav-link', function (e) {
-    e.preventDefault();
-    const page = $(this).data('page');
-    loadPage(page + '.html');
-});
-
+// Load page into #content
 function loadPage(file) {
     $('#content').load(file);
 }
 
+// Navigation clicks
+$(document).on('click', '.nav-link', function (e) {
+    e.preventDefault();
+    const page = $(this).data('page') + '.html';
+    loadPage(page);
+});
+
+// Initial page load + random banner
 $(window).on('load', function () {
+
+    const images = [
+        "/assets/images/banners/banner-fisciano.png",
+        "/assets/images/banners/banners-siena.png",
+        "/assets/images/banners/banners-skadar.png"
+    ];
+
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+
+    $(".banner-header").css("background-image",
+        `linear-gradient(rgba(248, 247, 242, 0.5), rgba(248, 247, 242, 0.8)), url('${randomImage}')`
+    );
+
+    // Load default page
     loadPage('aboutme.html');
 });
